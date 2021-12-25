1113d42b83d2abb4bb0489fb1144da9bc7c3b99e
erick <niteric@gmail.com>
2021-12-25 03:50:13 +0300

    edit the media url

--- Django_Portfolio/settings.py
+++ Django_Portfolio/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
+MEDIA_URL = os.path.join(BASE_DIR, '/media/')
 
 STATICFILES_DIRS = [
     BASE_DIR, 'static'
