39d733c8dcc29c97b3cb0506f1e28967a345652e
erick <niteric@gmail.com>
2022-12-29 07:21:11 +0300

    Update settings.py

--- Django_Portfolio/settings.py
+++ Django_Portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["erickmaina.pythonanywhere.com", "127.0.0.1",
                  '192.168.0.180', "mainaerick.herokuapp.com"]
